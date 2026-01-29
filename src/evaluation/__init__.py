41c4eb84b59f34b3381218bce97100e09d737d25
Adam Duffy-Castaldo <adam.duffy-castaldo@challoners.org>
2026-01-29 23:54:49 +0000

    chore(06.1-01): export bitrate functions from evaluation package
    - Add quantize_latent, dequantize_latent exports
    - Add estimate_channel_entropy, estimate_latent_entropy exports
    - Add compute_latent_bpp export

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -2,6 +2,13 @@ from .metrics import SARMetrics, compute_all_metrics
 from .evaluator import Evaluator, print_evaluation_report
 from .visualizer import Visualizer
 from .codec_baselines import Codec, JPEG2000Codec, JPEGCodec, CodecEvaluator
+from .bitrate import (
+    quantize_latent,
+    dequantize_latent,
+    estimate_channel_entropy,
+    estimate_latent_entropy,
+    compute_latent_bpp,
+)
 
 __all__ = [
     'SARMetrics',
@@ -13,4 +20,10 @@ __all__ = [
     'JPEG2000Codec',
     'JPEGCodec',
     'CodecEvaluator',
+    # Bitrate estimation
+    'quantize_latent',
+    'dequantize_latent',
+    'estimate_channel_entropy',
+    'estimate_latent_entropy',
+    'compute_latent_bpp',
 ]
